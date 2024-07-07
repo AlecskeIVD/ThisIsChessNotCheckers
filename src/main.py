edb0745f143ecadfdd9cbb03f7e4123620913f64
Alec Van Deuren <alec.vandeuren@student.kuleuven.be>
2024-07-07 21:19:41 +0200

    able to move pieces with mouse (slowly)

--- src/main.py
+++ src/main.py
@@ -15,11 +15,20 @@ from random import randint
 WINDOW = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Chess")
 
+# Dictionaries for translating pieces to human-readable names
+value_to_name = {1: "pawn", 3: "knight", 4: "bishop", 5: "rook", 8: "queen", 9: "king"}
+rgb_to_colour = {WHITE: "white", BLACK: "black"}
+index_to_column = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
+index_to_row = {}
+for i in range(8):
+    index_to_row[i] = 8-i
+
 
 def main(version: int = 0):
     run = True
     clock = pg.time.Clock()
-    gs = Gamestate( [Pawn((6, i), WHITE) for i in range(7)] + [Queen((7, 1), WHITE), King((7, 2), WHITE)], [King((5, 5), BLACK)])
+    gs = Gamestate([Pawn((6, i), WHITE) for i in range(7)] + [Queen((7, 1), WHITE), King((7, 2), WHITE)],
+                    [King((5, 5), BLACK)])
     # gs = Gamestate()
     selected_piece = None
     gs.draw_board(WINDOW)
@@ -27,51 +36,73 @@ def main(version: int = 0):
 
     while run:
         clock.tick(FPS)
-        print(gs.move)
-        if gs.move % 2 == 1:
-            lm = gs.legal_moves(WHITE)
-        else:
-            lm = gs.legal_moves(BLACK)
-        newmove = lm[randint(0, len(lm)-1)]
-        gs.update(newmove)
-        gs.draw_board(WINDOW)
-        pg.display.update()
-        if gs.white_wins():
-            run = False
-            print("White has won!")
-        elif gs.black_wins():
-            run = False
-            print("Black has won")
-        elif gs.stalemate():
-            run = False
-            print("Stalemate; no more legal moves")
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
-    time.sleep(5)
-    pg.quit()
-"""
+            if event.type == pg.KEYDOWN:
+                print(gs.move)
+                if gs.move % 2 == 1:
+                    lm = gs.legal_moves(WHITE)
+                else:
+                    lm = gs.legal_moves(BLACK)
+                newmove = lm[randint(0, len(lm) - 1)]
+                gs.update(newmove)
+                gs.draw_board(WINDOW)
+                pg.display.update()
+                if gs.white_wins():
+                    run = False
+                    print("White has won!")
+                elif gs.black_wins():
+                    run = False
+                    print("Black has won")
+                elif gs.stalemate():
+                    run = False
+                    print("Stalemate; no more legal moves")
             if event.type == pg.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
+                pos = pg.mouse.get_pos()
                 i = pos[1] // SQUAREWIDTH
                 j = pos[0] // SQUAREWIDTH
-                if not selected_piece:
+                if selected_piece is None:
                     selected_piece = gs.get_piece(i, j)
-                    generated_moves = selected_piece.generate_possible_moves()
-                    for move in generated_moves:
-                        pg.draw.circle(WINDOW, BLACK, ((move.j + 0.5)*SQUAREWIDTH, (move.i + 0.5)*SQUAREWIDTH), 0.1*SQUAREWIDTH)
-                        pg.display.update()
+                    if selected_piece is not None:
+                        print(f"Selected the {rgb_to_colour.get(selected_piece.colour)} "
+                              f"{value_to_name[selected_piece.value]} piece at {index_to_column[selected_piece.j]}"
+                              f"{index_to_row[selected_piece.i]}")
                 else:
-                    selected_piece.i = i
-                    selected_piece.j = j
+                    target_piece = None
+                    for piece in selected_piece.generate_possible_moves():
+                        if piece.i == i and piece.j == j:
+                            target_piece = piece
+                    if target_piece is None:
+                        selected_piece = None
+                    else:
+                        if selected_piece.colour == WHITE:
+                            target_gs = Gamestate([piece for piece in gs.white_pieces if piece != selected_piece]+[target_piece], gs.black_pieces, gs.move+1)
+                            if gs.is_legal(target_gs):
+                                gs.update(target_gs)
+                            else:
+                                selected_piece = None
+                        else:
+                            target_gs = Gamestate(gs.white_pieces,
+                                [piece for piece in gs.black_pieces if piece != selected_piece] + [target_piece],
+                                gs.move + 1)
+                            if gs.is_legal(target_gs):
+                                gs.update(target_gs)
+                            else:
+                                selected_piece = None
                     gs.draw_board(WINDOW)
                     pg.display.update()
-                    selected_piece = None
-                    print(gs.king_under_attack(WHITE))
-                    gs.move += 1
+                    if gs.white_wins():
+                        run = False
+                        print("White has won!")
+                    elif gs.black_wins():
+                        run = False
+                        print("Black has won")
+                    elif gs.stalemate():
+                        run = False
+                        print("Stalemate; no more legal moves")
 
     pg.quit()
-"""
 
 
 if __name__ == '__main__':
